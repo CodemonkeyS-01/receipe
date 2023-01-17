6f17a6b45f71bbfa16a43cd971d821f77a126e4a
CodemonkeyS-01 <121390080+CodemonkeyS-01@users.noreply.github.com>
2023-01-17 10:43:29 +0000

    removed rec_app/

--- receipe/receipe/urls.py
+++ receipe/receipe/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('rec_app', include("rec_app.urls"))
+    path('', include("rec_app.urls"))
 ]
