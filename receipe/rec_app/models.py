a98f9d478202befdf2b8ed34fd06bd5bc55015ae
CodemonkeyS-01 <121390080+CodemonkeyS-01@users.noreply.github.com>
2023-01-20 12:47:20 +0000

    add SQLite3

--- receipe/rec_app/models.py
+++ receipe/rec_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Receipe(models.Model):
+    name = models.CharField(max_length=64)
+    ingredients = models.CharField(max_length=500)
+    procedure = models.CharField(max_length = 1000)
--- receipe/rec_app/views.py
+++ receipe/rec_app/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from . models import Receipe
+
 
 # Create your views here.
 def index(request):
-    return render(request, "rec_app/index.html")
+    foods = Receipe.objects.all()
+    return render(request, "rec_app/index.html",{
+        "foods" :foods
+    })
+
+def receipe(request, receipe_id):
+    receipe = Receipe.objects.get(id=receipe_id)
+    return render(request, "rec_app/receipe.html",{
+        "receipe": receipe
+    })
