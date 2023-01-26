d5f5688315a864b0c1a0cce15e732383f40987c2
CodemonkeyS-01 <121390080+CodemonkeyS-01@users.noreply.github.com>
2023-01-26 16:56:28 +0000

    js & css file

---
+++ receipe/rec_app/static/rec_app/receipe.js
@@ -0,0 +1,12 @@
+console.log("connected")
+document.querySelector()
+function Topro(){
+    document.addEventListener('DOMContentLoaded', function(){
+        document.querySelectorAll('button').forEach(function(button){
+            button.onclick = function(){
+                console.log("working, no worries")
+            }})})
+    }
+    
+
+Topro()
